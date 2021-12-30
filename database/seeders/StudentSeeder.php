6298bf33f0885b0cc0cdcadab8ca5e1a411af02f
aliraza34 <alirazabscs007@gmail.com>
2021-12-30 23:45:18 +0500

    Data Added in DB using Seeder

--- database/seeders/StudentSeeder.php
+++ database/seeders/StudentSeeder.php
@@ -1,8 +1,11 @@
 <?php
 
 namespace Database\Seeders;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class StudentSeeder extends Seeder
 {
@@ -14,5 +17,12 @@ class StudentSeeder extends Seeder
     public function run()
     {
         //
+        DB::table('students')->insert([
+            'name'=>'Ali Raza',
+            'email'=>'ali@gmail.com',
+            'password'=>Hash::make('123'),
+
+        ]);
+
     }
 }
